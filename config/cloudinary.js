5665f76dbafe8416ab9c8d031e4f66fe41b166e3
sammyCyril <samuelehigale@gmail.com>
2025-05-27 22:45:46 -0700

    Update cloudinary.js
    testin commit

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,6 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 const dotenv = require('dotenv');
 dotenv.config();
+//gonna deletebsoon
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
